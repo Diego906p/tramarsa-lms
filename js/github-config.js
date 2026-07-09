a3b47618ae13580211230263b8808b50c46f733d
Diego906p <94724394+Diego906p@users.noreply.github.com>
2026-07-09 17:47:49 -0500

    Update GITHUB_TOKEN in github-config.js

--- js/github-config.js
+++ js/github-config.js
@@ -20,7 +20,7 @@
 export const GITHUB_OWNER = 'Diego906p';
 export const GITHUB_REPO = 'tramarsa-lms';
 export const GITHUB_BRANCH = 'main';
-export const GITHUB_TOKEN = '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';
+export const GITHUB_TOKEN = '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';
 
 export function githubEstaConfigurado() {
   return ![GITHUB_OWNER, GITHUB_REPO, GITHUB_TOKEN].some(v => v.startsWith('TU_'));
